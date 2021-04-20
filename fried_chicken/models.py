d03a750f0a402a51cc3be336c02d04bbd6394bb9
chloejiwon <chloe326o.o@gmail.com>
2021-04-20 22:21:57 +0900

    Add User model

--- fried_chicken/models.py
+++ fried_chicken/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    created_date = models.DateTimeField(blank=True, null=True)
+    
+    def create(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+

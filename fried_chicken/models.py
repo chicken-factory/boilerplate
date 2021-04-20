53c58dfa304b0eb4628d83d22743e5b280cb8b65
hayoung-lee <hayoung.lee@navercorp.com>
2021-04-20 22:21:04 +0900

    Add content class in model

--- fried_chicken/models.py
+++ fried_chicken/models.py
@@ -13,3 +13,19 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
+class Content(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    attachments = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
